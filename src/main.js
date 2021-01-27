066d47dbcd2b42667b77ef0373f8852a79ff9049
Nivm200 <nivm200@gmail.com>
2021-01-28 00:47:18 +0200

    Added error msg
    -Added an error message to let the user know when the inputs are incorrect.

--- src/main.js
+++ src/main.js
@@ -4,17 +4,6 @@ doneItemsCount = 0;
 let tasksDone = [];
 let tasksLeft = [];
 function addTask() {
-  /**
- * 
- *      <select id="priority-selector" name="number">
-        <option disabled selected value>importance</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>} event 
- */
   let task = document.getElementsByTagName("input")[0].value;
   let taskDiv = document.getElementById("tasks-container");
 
@@ -22,35 +11,36 @@ function addTask() {
   input.type = "checkbox";
   input.id = "item" + totalItemsCount;
   input.onclick = taskChecked;
-
-  //Find importance number
-  let priorityInput = document.getElementById("priority-selector").value;
-  if (priorityInput >= 1 && priorityInput <= 5) {
-    let label = document.createElement("label");
-    label.setAttribute("for", input.id);
-    label.innerText = task;
-
-    let div = document.createElement("div");
-
-    div.appendChild(input);
-    div.appendChild(label);
-
-    taskDiv.appendChild(div);
-
-    document.getElementsByTagName("input")[0].value = "";
-    totalItemsCount++;
-    todoItemsCount++;
-    if (todoItemsCount === 1) {
-      document.getElementById("empty-list-span").style.display = "none";
-    }
-    ////
-
-    console.log(getTaskAsObject(task, priorityInput, new Date()));
-
-    ////
+  let label = document.createElement("label");
+  label.setAttribute("for", input.id);
+  label.innerText = task;
+  if (task.length <= 1) {
+    alert("You need to add a task!");
   } else {
-    //If importance number is NOT chosen.
-    alert("You need to choose importance level!");
+    //Find importance number
+    let priorityInput = document.getElementById("priority-selector").value;
+    if (priorityInput >= 1 && priorityInput <= 5) {
+      let div = document.createElement("div");
+      document.getElementsByTagName("input")[0].value = "";
+      document.getElementsByTagName("select")[0].value = "";
+      div.appendChild(input);
+      div.appendChild(label);
+
+      taskDiv.appendChild(div);
+      console.log(document.getElementById("priority-selector"));
+
+      totalItemsCount++;
+      todoItemsCount++;
+      if (todoItemsCount === 1) {
+        document.getElementById("empty-list-span").style.display = "none";
+      }
+      ////
+      console.log(getTaskAsObject(task, priorityInput, new Date()));
+      ////
+    } else {
+      //If importance number is NOT chosen.
+      alert("You need to choose importance level!");
+    }
   }
 }
 
@@ -77,6 +67,7 @@ function taskChecked(event) {
   }
 }
 
+//My functions.
 function getTaskAsObject(text, priority, date) {
   return {
     text: text,
