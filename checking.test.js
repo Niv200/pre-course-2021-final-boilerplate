342391c967fa452a160ea6ff792af512e7508526
Nivm200 <nivm200@gmail.com>
2021-01-31 18:33:10 +0200

    Update checking.test.js

--- checking.test.js
+++ checking.test.js
@@ -1,7 +1,6 @@
 /**
  * @jest-environment node
  */
-const puppeteer = require("puppeteer");
 const full4s = require("@suvelocity/tester");
 const nock = require("nock");
 const useNock = require("nock-puppeteer");
@@ -12,13 +11,13 @@ let browser;
 
 const mockToDos = [
   {
-    text: "task testing",
-    priority: "4",
+    text: "first task input",
+    priority: "1",
     date: new Date(),
   },
   {
-    text: "task testing again",
-    priority: "3",
+    text: "second task input",
+    priority: "4",
     date: new Date(),
   },
 ];
@@ -66,7 +65,7 @@ describe(projectName, () => {
     browser = await puppeteer.launch({
       headless: false, // Uncomment me to see tests running in browser
       args: ["--disable-web-security"],
-      slowMo: 20, // Uncomment and change me to slow down tests speed in browser.
+      slowMo: 50, // Uncomment and change me to slow down tests speed in browser.
     });
     page = await browser.newPage();
     useNock(page, ["https://api.jsonbin.io/v3"]);
@@ -81,6 +80,15 @@ describe(projectName, () => {
   afterAll(async () => {
     await full4s.afterAll(projectName);
     await browser.close();
+  });
+
+  test("The todo list should be empty first", async () => {
+    await nock("https://api.jsonbin.io/v3").get(/.*/).reply(200, mocks.initBin);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+
+    const elements = await page.$$(".todo-text");
+    expect(elements.length).toBe(0);
   });
 
   test("Can add todo task with text and priority", async () => {
@@ -117,5 +125,130 @@ describe(projectName, () => {
     expect(elements.length).toBe(1);
     expect(firstItem).toBe(firstTaskText);
     expect(firstItemPriority).toBe(firstTaskPriority);
+  });
+
+  test("After add task the input should be empty", async () => {
+    const mockToDo = mockToDos[1];
+    const secondTaskText = mockToDo.text;
+    const secondTaskPriority = mockToDo.priority;
+
+    await nock("https://api.jsonbin.io/v3")
+      .get(/.*/)
+      .reply(200, mocks.toDoAddResponse);
+
+    await nock("https://api.jsonbin.io/v3")
+      .put(/.*/, () => true)
+      .reply(200, mocks.toDoAddSecondResponse);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+
+    await page.waitForSelector("#text-input");
+
+    await page.type("#text-input", secondTaskText);
+    await page.select("#priority-selector", secondTaskPriority);
+    await page.click("#add-button");
+    const inputElement = await page.$("#text-input");
+    const currentInput = await (
+      await inputElement.getProperty("value")
+    ).jsonValue();
+    expect(currentInput).toBe("");
+  });
+
+  test("Task should be added in the end of the list", async () => {
+    await nock("https://api.jsonbin.io/v3")
+      .get(/.*/)
+      .reply(200, mocks.toDoAddSecondResponse);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+
+    const mockToDo = mockToDos[1];
+
+    await page.waitForSelector(".todo-text");
+    const elements = await page.$$(".todo-text");
+    const secondItem = await (
+      await elements[1].getProperty("innerText")
+    ).jsonValue();
+
+    const priorityElements = await page.$$(".todo-priority");
+    const secondItemPriority = await (
+      await priorityElements[1].getProperty("innerText")
+    ).jsonValue();
+    expect(secondItem).toBe(mockToDo.text);
+    expect(secondItemPriority).toBe(mockToDo.priority);
+  });
+
+  test("Counter increase", async () => {
+    await nock("https://api.jsonbin.io/v3")
+      .get(/.*/)
+      .reply(200, mocks.toDoAddSecondResponse);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+
+    await page.waitForSelector(".todo-container");
+
+    const counterElement = await page.$("#counter");
+    const currentCounter = await (
+      await counterElement.getProperty("innerText")
+    ).jsonValue();
+    expect(currentCounter).toBe("2");
+  });
+
+  test("Can sort by priority", async () => {
+    await nock("https://api.jsonbin.io/v3")
+      .get(/.*/)
+      .reply(200, mocks.toDoAddSecondResponse);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+    const mockToDo = mockToDos[1];
+
+    await page.click("#sort-button");
+    const elements = await page.$$(".todo-text");
+    const secondItem = await (
+      await elements[0].getProperty("innerText")
+    ).jsonValue();
+    const priorityElements = await page.$$(".todo-priority");
+    const secondItemPriority = await (
+      await priorityElements[0].getProperty("innerText")
+    ).jsonValue();
+    expect(secondItem).toBe(mockToDo.text);
+    expect(secondItemPriority).toBe(mockToDo.priority);
+  });
+
+  test("If data structure is correct in localStorage", async () => {
+    const dataInLocalStorage = await page.evaluate(() => {
+      return localStorage.getItem("my-todo");
+    });
+
+    if (!dataInLocalStorage) {
+      expect(dataInLocalStorage).toEqual(null);
+      return;
+    }
+
+    for (let taskObj of JSON.parse(dataInLocalStorage)) {
+      expect(taskObj.text).not.toBe(undefined);
+      expect(taskObj.priority).not.toBe(undefined);
+      expect(taskObj.date).not.toBe(undefined);
+    }
+  });
+
+  test("Fetches GET JSONBIN.io", async () => {
+    await nock("https://api.jsonbin.io/v3")
+      .get(/.*/)
+      .reply(200, mocks.fetchTest);
+
+    await page.goto(path, { waitUntil: "networkidle0" });
+
+    await page.waitForSelector(".todo-container");
+
+    const textNode = await page.$$(".todo-text");
+    const priorityNode = await page.$$(".todo-priority");
+
+    const text = await (await textNode[0].getProperty("innerText")).jsonValue();
+    const priority = await (
+      await priorityNode[0].getProperty("innerText")
+    ).jsonValue();
+
+    expect(text).toBe(mocks.fetchTest.record["my-todo"][0].text);
+    expect(priority).toBe(mocks.fetchTest.record["my-todo"][0].priority);
   });
 });
